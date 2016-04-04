fc6ed0653f29461b3f7225dac1cb773afbb808f9
laudeon <louis.audeon@mail.be>
2016-04-04 21:45:25 +0200

    initial commit, with proxy settings script

---
+++ proxy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Proxy settings prompt
+echo -e '\n----------------------\n Proxy settings (with credentials) \n--------------------\n'
+read -p 'Proxy - login: ' loginProxy
+read -p 'Proxy - password: ' passwdProxy
+read -p 'Proxy - host: ' hostProxy
+read -p 'Proxy - port: ' portProxy
+
+#global proxy settings
+proxy="http://$loginProxy"":""$passwdProxy""@""$host"":""$port""/"
+echo "export http_proxy=$proxy" >> /home/pi/.bashrc
+echo "export https_proxy=$proxy" >> /home/pi/.bashrc
+
+#apt proxy setting
+sudo touch /etc/apt/apt.conf.d/99proxy
+aptProxy="Acquire::http::Proxy \"$proxy\";"
+echo $aptProxy | sudo tee -a /etc/apt/apt.conf.d/99proxy > /dev/null
+
+#Update pi
+sudo apt-get update && sudo apt-get upgrade
+
+#DONE
+echo "\n--------------------\n done, restart your pi with 'sudo reboot' \n---------------------\n"
